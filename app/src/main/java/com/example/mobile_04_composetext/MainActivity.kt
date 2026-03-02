e7d3df6fd1290ed4d169f6d6375e13eaa3b737cf
EDVAN ALVES DE OLIVEIRA <25132698@dsn127m1246731.cfp127aluno.local>
2026-03-02 11:26:18 -0300

    feat: add Textfield for textinput and numberinput

--- app/src/main/java/com/example/mobile_04_composetext/MainActivity.kt
+++ app/src/main/java/com/example/mobile_04_composetext/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mobile_04_composetext
 
-import android.R.attr.value
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,16 +9,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.mobile_04_composetext.ui.theme.MOBILE_04_ComposeTextTheme
 
@@ -63,10 +65,28 @@ fun BasicComponentScreen(modifier: Modifier = Modifier) {
 //            fontFamily = patrickFont,
             color = Color(80, 67, 89, 255)
         )
-        var fieldInput = ""
+        //Estrutura para mudar o estado e lembrar da mudança
+        val nameField = remember {
+            mutableStateOf("Android")
+        }
         TextField(
-            value = "",
-            onValueChange = {}
+            value = nameField.value,
+            onValueChange = { newInput ->
+//                Log.i("Teste", newInput) // Log é um baita recurso para observar mudanças de estado
+                nameField.value = newInput
+            }
+        )
+
+        var ageField = remember {
+            mutableStateOf("80085")
+        }
+        TextField(
+            value = ageField.value,
+            onValueChange = { newInput ->
+//                Log.i("Teste", newInput) // Log é um baita recurso para observar mudanças de estado
+                ageField.value = newInput
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
     }
 }
